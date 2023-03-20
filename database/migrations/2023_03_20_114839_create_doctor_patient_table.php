301c29a27faf615615762da29fb638f90b777109
Jenda <jan.illetsko@gmail.com>
2023-03-20 13:06:57 +0100

    created tables for database

---
+++ database/migrations/2023_03_20_114839_create_doctor_patient_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('doctor_patient', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId("doctor_id");
+            $table->foreignId("patient_id");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('doctor_patient');
+    }
+};
