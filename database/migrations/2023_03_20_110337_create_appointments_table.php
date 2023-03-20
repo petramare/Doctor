3e521d620b3fcfe54002fd54fffd2ada7ef39ad3
Mario <lartu@seznam.cz>
2023-03-20 13:14:24 +0100

    appointment + appointment statuses tables

---
+++ database/migrations/2023_03_20_110337_create_appointments_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('appointments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('doctor_id');
+            $table->foreignId('patient_id');
+            $table->dateTime('start', 0);
+            $table->dateTime('end', 0);
+            $table->string('description');
+            $table->foreignId('status_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('appointments');
+    }
+};
