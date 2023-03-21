8d9aa57402386f5fcacb7d844b7e4baeb43a6771
Petra Marešová <petra.maresov@gmail.com>
2023-03-21 11:04:56 +0100

    registration without existing path

--- resources/js/doctor/Components/RegisterUser/RegisterUser.jsx
+++ resources/js/doctor/Components/RegisterUser/RegisterUser.jsx
@@ -5,8 +5,53 @@ import React, { useState, useEffect, useContext } from 'react';
 
 export default function RegisterUser(){
 
+    const { getUserInformation } = useContext(UserContext);
+    const [values, setValues] = useState({
+        
+        first_name: '',
+        surname: '',
+        name: '',
+        email: '',
+        date_of_birth:'',
+        id_number: '',
+        password: '',
+        password_confirmation: ''
+    })
+
+    const handleSubmit = async (event) => {
+
+        event.preventDefault();
+
+        // make the AJAX request
+        const response = await fetch('/register', {
+            method: 'POST',
+            body: JSON.stringify(values),
+            headers: {
+                'Accept': 'application/json',
+                'Content-type': 'application/json',
+                'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content')
+            }
+        });
+
+    const handleChange = (event) => {
+        setValues(previous_values => {         //values before update
+            return ({...previous_values,        //values to be updated
+                [event.target.name]: event.target.value
+            });
+        });
+    }
+
+
     return (
         <form action="/register" method="post" onSubmit={ handleSubmit }>
+
+            First name:<br />
+            <input type="text" name="first_name" value={ values.first_name } onChange={ handleChange } />
+            <br /><br />
+
+            SurName:<br />
+            <input type="text" name="surname" value={ values.surname } onChange={ handleChange } />
+            <br /><br />
 
             Name:<br />
             <input type="text" name="name" value={ values.name } onChange={ handleChange } />
@@ -14,6 +59,14 @@ export default function RegisterUser(){
 
             Email:<br />
             <input type="email" name="email" value={ values.email } onChange={ handleChange } />
+            <br /><br />
+
+            Date of birth:<br />
+            <input type="string" name="date_of_birth" value={ values.date_of_birth } onChange={ handleChange } />
+            <br /><br />
+            
+            Id number:<br />
+            <input type="number" name="id_number" value={ values.id_number } onChange={ handleChange } />
             <br /><br />
 
             Password:<br />
@@ -29,3 +82,4 @@ export default function RegisterUser(){
         </form>
     );
 }
+};
