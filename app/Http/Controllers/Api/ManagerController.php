7aada80928f1b5cc4949bc7eb1a6b4cc78fd1e4c
Mario <lartu@seznam.cz>
2023-03-21 22:06:19 +0100

    trying work on update method and value read only issue

--- app/Http/Controllers/Api/ManagerController.php
+++ app/Http/Controllers/Api/ManagerController.php
@@ -20,4 +20,27 @@ class ManagerController extends Controller
         $manager = Manager::where('manager_id', $manager_id)->with(['user', 'clinic'])->first();
         return $manager;
     }
+
+    public function update(Request $request)
+    {
+
+
+        //-taken from MI6
+        // $mission = Mission::findOrFail($request->input('id'));
+
+        // $originalOutcome = $mission->outcome;
+
+        // $mission->name = $request->input('name');
+        // $mission->year = $request->input('year');
+        // $mission->outcome = $request->input('outcome');
+
+        // $mission->save();
+
+        // $admins = User::where('role', 'admin')->get();
+        // if ($originalOutcome != $request->input('outcome')) {
+        //     Notification::send($admins, new MissionOutcomeUpdated($mission));
+        // }
+
+        // return ['message' => 'Succesfully saved'];
+    }
 }
--- resources/js/doctor/Components/Content/Manager/EditInfo/EditInfo.jsx
+++ resources/js/doctor/Components/Content/Manager/EditInfo/EditInfo.jsx
@@ -5,6 +5,7 @@ import { useParams } from "react-router-dom";
 export default function EditInfo() {
     let { id } = useParams();
     const [managerDetail, setManagerDetail] = useState(null);
+    const [firstName, setFirstName] = useState({ first_name: "lukas" });
 
     const loadManager = async () => {
         try {
@@ -25,131 +26,131 @@ export default function EditInfo() {
         });
     };
 
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        console.log("submited");
+        console.log(managerDetail);
+
+        // try {
+        //     let response = await axios.post('/api/missions/store', mission)
+        //     setMessage(response.data['message'])
+        // } catch (error) {
+        //     console.log(error)
+        // }
+    };
+
     return managerDetail ? (
-        <div className="col-md-5 border-right">
-            <div className="p-3 py-5">
-                <div className="d-flex justify-content-between align-items-center mb-3">
-                    <form action="">
+        <div className="container">
+            <div className="row">
+                <div className="col">
+                    <form action="" method="post" onSubmit={handleSubmit}>
                         <h1>User Info</h1>
-                        <div className="row">
-                            <div className="col-md-6 offset-md-3">
-                                <label htmlFor="first_name">First Name:</label>
-                                <input
-                                    type="text"
-                                    name="first_name"
-                                    value={managerDetail.user.first_name}
-                                    onChange={handleChange}
-                                />
-                            </div>
+                        <div className="form-group">
+                            <label htmlFor="first_name">First Name:</label>
+                            <input
+                                type="text"
+                                name="first_name"
+                                className="form-control"
+                                value={managerDetail.user.first_name}
+                                onChange={handleChange}
+                            />
                         </div>
-                        <div className="row">
-                            <div className="col-md-6 offset-md-3">
-                                <label htmlFor="surname">Surname:</label>
-                                <input
-                                    type="text"
-                                    name="first_name"
-                                    value={managerDetail.user.surname}
-                                    onChange={handleChange}
-                                />
-                            </div>
+                        <div className="form-group">
+                            <label htmlFor="surname">Surname:</label>
+                            <input
+                                type="text"
+                                name="first_name"
+                                className="form-control"
+                                value={managerDetail.user.surname}
+                                onChange={handleChange}
+                            />
                         </div>
-                        <div className="row">
-                            <div className="col-md-6 offset-md-3">
-                                <label htmlFor="email">Email:</label>
-                                <input
-                                    type="string"
-                                    name="email"
-                                    value={managerDetail.user.email}
-                                    onChange={handleChange}
-                                />
-                            </div>
+                        <div className="form-group">
+                            <label htmlFor="email">Email:</label>
+                            <input
+                                type="string"
+                                name="email"
+                                className="form-control"
+                                value={managerDetail.user.email}
+                                onChange={handleChange}
+                            />
                         </div>
-                        <div className="row">
-                            <div className="col-md-6 offset-md-3">
-                                {" "}
-                                <label htmlFor="date_of_birth">
-                                    Date of Birth:
-                                </label>
-                                <input
-                                    type="text"
-                                    name="date_of_birth"
-                                    value={managerDetail.user.date_of_birth}
-                                    onChange={handleChange}
-                                />
-                            </div>
+                        <div className="form-group">
+                            <label htmlFor="date_of_birth">
+                                Date of Birth:
+                            </label>
+                            <input
+                                type="text"
+                                name="date_of_birth"
+                                className="form-control"
+                                value={managerDetail.user.date_of_birth}
+                                onChange={handleChange}
+                            />
                         </div>
-                        <div className="row">
-                            <div className="col-md-6 offset-md-3">
-                                {" "}
-                                <label htmlFor="role">Role:</label>
-                                <input
-                                    type="text"
-                                    name="role"
-                                    value={managerDetail.user.role}
-                                    onChange={handleChange}
-                                />
-                            </div>
+                        <div className="form-group">
+                            <label htmlFor="role">Role:</label>
+                            <input
+                                type="text"
+                                name="role"
+                                className="form-control"
+                                value={managerDetail.user.role}
+                                onChange={handleChange}
+                            />
                         </div>
                         <div className="">
                             <h1>Clinic Info</h1>
-                            <div className="row">
-                                <div className="col-md-6 offset-md-3">
-                                    {" "}
-                                    <label htmlFor="name">Role:</label>
-                                    <input
-                                        type="text"
-                                        name="name"
-                                        value={managerDetail.clinic.name}
-                                        onChange={handleChange}
-                                    />
-                                </div>
+                            <div className="form-group">
+                                <label htmlFor="name">Role:</label>
+                                <input
+                                    type="text"
+                                    name="name"
+                                    className="form-control"
+                                    value={managerDetail.clinic.name}
+                                    onChange={handleChange}
+                                />
                             </div>
-                            <div className="row">
-                                <div className="col-md-6 offset-md-3">
-                                    {" "}
-                                    <label htmlFor="address">Address:</label>
-                                    <input
-                                        type="text"
-                                        name="address"
-                                        value={managerDetail.clinic.address}
-                                        onChange={handleChange}
-                                    />
-                                </div>
+                            <div className="form-group">
+                                <label htmlFor="address">Address:</label>
+                                <input
+                                    type="text"
+                                    name="address"
+                                    className="form-control"
+                                    value={managerDetail.clinic.address}
+                                    onChange={handleChange}
+                                />
                             </div>
-                            <div className="row">
-                                <div className="col-md-6 offset-md-3">
-                                    {" "}
-                                    <label htmlFor="registration_code">
-                                        Registration Code:
-                                    </label>
-                                    <input
-                                        type="text"
-                                        name="registration_code"
-                                        value={
-                                            managerDetail.clinic
-                                                .registration_code
-                                        }
-                                        onChange={handleChange}
-                                    />
-                                </div>
+                            <div className="form-group">
+                                <label htmlFor="registration_code">
+                                    Registration Code:
+                                </label>
+                                <input
+                                    type="text"
+                                    name="registration_code"
+                                    className="form-control"
+                                    value={
+                                        managerDetail.clinic.registration_code
+                                    }
+                                    onChange={handleChange}
+                                />
                             </div>
-                            <div className="row">
-                                <div className="col-md-6 offset-md-3">
-                                    {" "}
-                                    <label htmlFor="tax_registration_code">
-                                        Tax Registration Code:
-                                    </label>
-                                    <input
-                                        type="text"
-                                        name="tax_registration_code"
-                                        value={
-                                            managerDetail.clinic
-                                                .tax_registration_code
-                                        }
-                                        onChange={handleChange}
-                                    />
-                                </div>
+                            <div className="form-group">
+                                <label htmlFor="tax_registration_code">
+                                    Tax Registration Code:
+                                </label>
+                                <input
+                                    type="text"
+                                    name="tax_registration_code"
+                                    className="form-control"
+                                    value={
+                                        managerDetail.clinic
+                                            .tax_registration_code
+                                    }
+                                    onChange={handleChange}
+                                />
                             </div>
+                            <button type="submit" className="btn btn-primary">
+                                Update
+                            </button>
                         </div>
                     </form>
                 </div>
