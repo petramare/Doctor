6cb0e663eb6a1094fa8588c70d6a5cb85d20aae1
Jan Sebesta <jan.sebesta88@gmail.com>
2023-03-21 13:24:08 +0100

    updated folders

---
+++ resources/js/doctor/Components/Content/Patient/Home/PatientHome.jsx
@@ -0,0 +1,12 @@
+export default function PatientHome() {
+
+    return (
+        <>
+            <div className="container">
+                <div className="row">
+
+                </div>
+            </div>
+        </>
+    )
+}
