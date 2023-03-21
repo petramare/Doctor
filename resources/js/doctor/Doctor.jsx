caf4f927d175d95ad3cd93c760e0a1ac1856255f
Petra Marešová <petra.maresov@gmail.com>
2023-03-21 10:09:35 +0100

    base for registeruser

--- resources/js/doctor/Doctor.jsx
+++ resources/js/doctor/Doctor.jsx
@@ -2,6 +2,7 @@
 import Navbar from './Components/navbar/Navbar';
 import Footer from "./Components/Footer/Footer"
 import LandingPage from "./Components/LandingPage/Landingpage"
+import RegisterUser from './Components/RegisterUser/RegisterUser';
 
 function Doctor() {
 
