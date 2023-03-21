6238b45135ec7858ef02fef031fd4073031e706e
Jan Sebesta <jan.sebesta88@gmail.com>
2023-03-21 11:26:53 +0100

    Routes updated

--- resources/js/doctor/Doctor.jsx
+++ resources/js/doctor/Doctor.jsx
@@ -1,7 +1,6 @@
 import Content from './Components/Content/Content';
 import Navbar from './Components/navbar/Navbar';
 import Footer from "./Components/Footer/Footer";
-import LandingPage from "./Components/LandingPage/Landingpage";
 import { BrowserRouter } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import UserContext from './Components/UserContext/UserContext';
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\IndexController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,8 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-return view('welcome');
-});
+// Route::get('/', function () {
+// return view('welcome');
+// });
 
-
+Route::get('/{path?}', [IndexController::class, 'index'])->where('path', '.*')->name('homepage');
