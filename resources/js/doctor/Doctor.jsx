84ee2cc8e4d5819bfaa994e7ce10e9d9c50bfb5c
Jan Sebesta <jan.sebesta88@gmail.com>
2023-03-21 10:07:14 +0100

    login page and UserContext file

--- resources/js/doctor/Doctor.jsx
+++ resources/js/doctor/Doctor.jsx
@@ -1,16 +1,25 @@
 
 import Navbar from './Components/navbar/Navbar';
-import Footer from "./Components/Footer/Footer"
-import LandingPage from "./Components/LandingPage/Landingpage"
+import Footer from "./Components/Footer/Footer";
+import LandingPage from "./Components/LandingPage/Landingpage";
+import { BrowserRouter } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import UserContext from './Components/UserContext/UserContext';
 
 function Doctor() {
 
+    const [user, setUser] = useState(null);
+
     return (
-        <div>
-            <Navbar />
-            <LandingPage />
-            <Footer />
-        </div>
+        <UserContext.Provider value={{ user, setUser }}>
+            <BrowserRouter>
+
+                <Navbar />
+                <LandingPage />
+                <Footer />
+
+            </BrowserRouter>
+        </UserContext.Provider>
     )
 }
 
