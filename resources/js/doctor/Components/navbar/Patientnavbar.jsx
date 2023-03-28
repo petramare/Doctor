450e39202fe46bca806a365852c2b4a84dfdcbc1
Petra Marešová <petra.maresov@gmail.com>
2023-03-28 16:07:22 +0200

    navbar pacienta v puvodnim stavu

--- resources/js/doctor/Components/navbar/Patientnavbar.jsx
+++ resources/js/doctor/Components/navbar/Patientnavbar.jsx
@@ -4,40 +4,36 @@ import Logout from "../../Logout/Logout";
 export default function Patientnavbar() {
     return (
         <>
-            <nav className="navbar-nav nav-justified nav-pills flex-row">
-                
-                    <li className="nav-item active">
-                        <Link to="/patient" className="nav-link">
-                            Home
-                        </Link>
-                    </li>
-                    <li className="nav-item active">
-                        <Link to="/patient/edit" className="nav-link">
-                            Edit info
-                        </Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to="/patient/find" className="nav-link">
-                            Find Doctor
-                        </Link>
-                    </li>
-                    
-                    <li className="nav-item">
-                        <Link to="patient/condition" className="nav-link">
-                            Health condition
-                        </Link>
-                    </li>
-        
-                      <li className="nav-item">
-                        <Link to="/patient/messages" className="nav-link">
-                            Messages
-                        </Link>
-                    </li>
-                    <li className="nav-item">
-                        <Logout />
-                    </li>
-            </nav>
-    
+            <li className="nav-item active">
+                <Link to="/patient" className="nav-link">
+                    Home
+                </Link>
+            </li>
+            <li className="nav-item active">
+                <Link to="/patient/edit" className="nav-link">
+                    Edit info
+                </Link>
+            </li>
+            <li className="nav-item">
+                <Link to="/patient/find" className="nav-link">
+                    Find Doctor
+                </Link>
+            </li>
+            
+            <li className="nav-item">
+                <Link to="patient/condition" className="nav-link">
+                    Health condition
+                </Link>
+            </li>
+
+                <li className="nav-item">
+                <Link to="/patient/messages" className="nav-link">
+                    Messages
+                </Link>
+            </li>
+            <li className="nav-item">
+                <Logout />
+            </li>
         </>
     );
 }
