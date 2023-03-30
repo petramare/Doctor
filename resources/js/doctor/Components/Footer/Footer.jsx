d4b7aa0a461c3107984a8b9318fbed5bd67fb3df
Jenda <jan.illetsko@gmail.com>
2023-03-30 15:57:28 +0200

    footer done

--- resources/js/doctor/Components/Footer/Footer.jsx
+++ resources/js/doctor/Components/Footer/Footer.jsx
@@ -2,40 +2,28 @@ import { Link } from "react-router-dom";
 
 export default function Footer() {
     return (
-        <div className="container">
-            <footer className="py-3 my-4">
-                <ul className="nav justify-content-center border-bottom pb-3 mb-3">
-                    <li className="nav-item">
-                        <a className="nav-link px-2 text-muted" href="#">
-                            Home
-                        </a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link px-2 text-muted" href="#">
-                            About us
-                        </a>
-                    </li>
-                    <li className="nav-item active">
-                        <Link to="/about-us" className="nav-link">
-                            About Us
-                        </Link>
-                    </li>
-                    <li className="nav-item active">
-                        <Link to="/techstack" className="nav-link">
-                            Tech Stack
-                        </Link>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link px-2 text-muted" href="#">
-                            Pricing
-                        </a>
-                    </li>
-                </ul>
+        <footer className="py-3">
+            <ul className="nav justify-content-center border-bottom pb-3 mb-3">
+                <li className="nav-item">
+                    <Link to="/" className="nav-link">
+                        Home
+                    </Link>
+                </li>
+                <li className="nav-item active">
+                    <Link to="/about-us" className="nav-link">
+                        About Us
+                    </Link>
+                </li>
+                <li className="nav-item active">
+                    <Link to="/techstack" className="nav-link">
+                        Tech Stack
+                    </Link>
+                </li>
+            </ul>
 
-                <p className="text-center text-muted">
-                    Copyright &#169; CalenDr. All rights reserved.
-                </p>
-            </footer>
-        </div>
+            <p className="text-center text-light">
+                Copyright &#169; CalenDr. All rights reserved.
+            </p>
+        </footer>
     );
 }
