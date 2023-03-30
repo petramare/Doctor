4b7fd0806501ed9582cb90d6111b18b35872bbe3
Mario <lartu@seznam.cz>
2023-03-30 13:58:10 +0200

    every item in footer

---
+++ resources/js/doctor/Components/Footer/LessonsLearnt/LessonsLearnt.jsx
@@ -0,0 +1,20 @@
+export default function LessonsLearnt() {
+    return (
+        <>
+            <h1>Lessons Learnt</h1>
+            <iframe
+                width={500}
+                height={500}
+                src="https://embed.lottiefiles.com/animation/138605"
+            ></iframe>
+            <div className="col">
+                <div>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Aliquid laborum nisi esse non illo rem repudiandae eveniet
+                    at saepe. Ducimus tempora natus repellat fuga eos iste in
+                    facere quasi porro.
+                </div>
+            </div>
+        </>
+    );
+}
