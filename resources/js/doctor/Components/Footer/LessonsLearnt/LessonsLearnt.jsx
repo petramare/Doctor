6646d69978e86c9b446bcf2c6ee1006ec5197665
Mario <lartu@seznam.cz>
2023-03-30 15:03:12 +0200

    footer articles

--- resources/js/doctor/Components/Footer/LessonsLearnt/LessonsLearnt.jsx
+++ resources/js/doctor/Components/Footer/LessonsLearnt/LessonsLearnt.jsx
@@ -1,19 +1,57 @@
 export default function LessonsLearnt() {
     return (
         <>
-            <h1>Lessons Learnt</h1>
-            <iframe
-                width={500}
-                height={500}
-                src="https://embed.lottiefiles.com/animation/138605"
-            ></iframe>
-            <div className="col">
-                <div>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Aliquid laborum nisi esse non illo rem repudiandae eveniet
-                    at saepe. Ducimus tempora natus repellat fuga eos iste in
-                    facere quasi porro.
+            <section id="lessons-learned" className="bg-light py-5">
+                <div className="container">
+                    <h2 className="text-center mb-5">Lessons Learned</h2>
+                    <div className="row">
+                        <div className="col-lg-4">
+                            <div className="card mb-4">
+                                <div className="card-body">
+                                    <p className="lh-lg">
+                                        <blockquote className="blockquote mb-0">
+                                            I believe we have made some weird
+                                            choices in DB desing.
+                                        </blockquote>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-lg-4">
+                            <div className="card mb-4">
+                                <div className="card-body">
+                                    <p className="lh-lg">
+                                        <blockquote className="blockquote mb-0">
+                                            Hey guys - so have we really been
+                                            doing the same thing each of us for
+                                            the last 8 hours?
+                                        </blockquote>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-lg-4">
+                            <div className="card mb-4">
+                                <div className="card-body">
+                                    <p>
+                                        <blockquote className="blockquote mb-0">
+                                            Make sure to include this file in
+                                            the gitignore - you mean the one
+                                            I've just merged to main?
+                                        </blockquote>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
+            </section>
+            <div className="container d-flex justify-content-center">
+                <iframe
+                    width={500}
+                    height={500}
+                    src="https://embed.lottiefiles.com/animation/138605"
+                ></iframe>
             </div>
         </>
     );
